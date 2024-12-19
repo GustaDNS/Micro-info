793067faeb139c4154c6fb28d3e03578e4313262
Gustavo <88571393+GustaDNS@users.noreply.github.com>
2024-12-19 19:37:45 +0000

    inputs

---
+++ Python/inputs/inputs.py
@@ -0,0 +1,20 @@
+#exercise 1
+print("\nExercise 1!")
+
+name = input("Qual é seu nome? ")
+print(f"Olá, {name}!")
+
+#exercise 2
+print("Exercise 2!")
+
+name_read = input("Qual é o primeiro nome: ")
+name_read2 = input("Qual é o segundo nome: ")
+print(f"\n{name_read}, roubou pão na casa do {name_read2}!")
+print(f"{name_read2} ficou triste e com fome,")
+print("Porque o bandeijão estava fechado.\n")
+
+#exercise 3
+print("Exercise 3!")
+
+x = input("qualquer frase: ")
+print(x[::-1], "\n")
